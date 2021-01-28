02bb9c0bc48444d83e803e9390f5aac2cce4753c
Fairy_Addict <nyashaperrry2005@yahoo.com>
2021-01-28 09:49:52 -0500

    commit tree 88d6d74f6bce68ec84a673db1c9ba1bb965a6adb

--- lib/school.rb
+++ lib/school.rb
@@ -1,26 +1,7 @@
 class School
-    attr_accessor :name, :roster
+attr_accessor :roster
 
-    def initialize(name)
-        @name = name
+    def initialize(roster)
         @roster = {}
+    end
 end 
-
-def add_student(name, grade)
-    roster[grade] ||= [] #we need something that is going to set the has value equal to the key
-    roster[grade] << name
-end
-
-def grade(student_grade)
-    roster[student_grade]
-end
-
-#find a method that sorts the students names (name.sort) => how to write out this method
-def sort
-    new_hash = {}
-    roster.each do |grade, name|
-        new_hash[grade] = name.sort
-end
-new_hash
-end
-end
